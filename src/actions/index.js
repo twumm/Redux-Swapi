d591047521c7b212fffbe60b335fb2f71fff5487
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-19 12:52:19 +0000

    feature: Create methods for triggering the various case of the reducer Create method to fetch data from the server async

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,3 +11,41 @@ import axios from 'axios';
 export const FETCHING = 'FETCHING';
 export const SUCCESS = 'SUCCESS';
 export const FAILURE = 'FAILURE';
+
+const getCharacters = (characters) => {
+  return {
+    type: SUCCESS,
+    payload: characters,
+  }
+}
+
+const setFailure = (error) => {
+  return {
+    type: FAILURE,
+    payload: error,
+  }
+}
+
+const setFetchingState = (status) => {
+  return {
+    type: FETCHING,
+    payload: status,
+  }
+}
+
+export const fetchCharacters = () => dispatch => {
+  debugger
+  dispatch(setFetchingState(true));
+  axios.get('https://swapi.co/api/people')
+    .then(response => {
+      console.log('response here' + response.data.results)
+      dispatch(getCharacters(response.data))
+    })
+    .catch(error => {
+      debugger;
+      dispatch(setFailure(error.message))
+    })
+    .finally(() => {
+      dispatch(setFetchingState(false));
+    })
+}
